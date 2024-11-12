d41937974e1c7fe47a8a18db2b28df5b2d8258cb
Gerard-007 <gerardnwazk@gmail.com>
2024-11-12 19:45:46 +0100

    Done from task 1 to 9 with test all working

--- Assignments/12-11-2024/index.js
+++ Assignments/12-11-2024/index.js
@@ -10,5 +10,79 @@ const squareElements = (array) => {
     return array.map(element => element * element);
 }
 
+const bookDistribution = (array) => {
+    let result = [];
+    for (let member of array) {
+        let obj = {
+            name: member,
+            status: "Delivered"
+        };
+        result.push(obj);
+    }
+    return result;
+};
 
-module.exports = {filterTestScores, increaseByFivePoints, squareElements}
+const studentPlanningSchedule = (array) => {
+    return array.filter((element) => element.includes("PM"));
+}
+
+const studentExpenseCalculator = (object) => {
+    let result = 0
+    for (let item in object) {
+        result += object[item];
+    }
+    return result;
+}
+
+const calculateStudentGrade = (array) => {
+    let result = [];
+    array.map(score => {
+        if (score >= 90 && score <= 100) {
+            result.push("A");
+        } else if (score >= 80 && score <= 89) {
+            result.push("B");
+        } else if (score >= 70 && score <= 79) {
+            result.push("C");
+        } else if (score >= 60 && score <= 69) {
+            result.push("D");
+        } else if (score < 60) {
+            result.push("F")
+        }
+    })
+    return result
+}
+
+const shoppingList = (array) => {
+    let result = [];
+    array.filter((item) => {
+        if(item.isHealthy === true) result.push(item.name);
+    })
+    return result;
+}
+
+const customerOrderAnalyzer = (array) => {
+    let result = [];
+    array.forEach((obj) => {
+        let total = 0;
+        obj.items.forEach((item) => {
+            total += item.price;
+        })
+        if (total >= 100) result.push({
+            id: obj.id,
+            total: total,
+        });
+    })
+    return result;
+}
+
+module.exports = {
+    customerOrderAnalyzer,
+    shoppingList,
+    calculateStudentGrade,
+    studentExpenseCalculator,
+    studentPlanningSchedule,
+    filterTestScores,
+    increaseByFivePoints,
+    squareElements,
+    bookDistribution
+}
--- Assignments/12-11-2024/task.test.js
+++ Assignments/12-11-2024/task.test.js
@@ -1,4 +1,4 @@
-const {filterTestScores, increaseByFivePoints, squareElements} = require('./index');
+const {customerOrderAnalyzer, shoppingList, calculateStudentGrade, studentExpenseCalculator, studentPlanningSchedule, bookDistribution, filterTestScores, increaseByFivePoints, squareElements} = require('./index');
 
 test("Filter scores more than 70", () => {
     const array = [20, 70, 90, 80];
@@ -18,5 +18,67 @@ test("Squaring elements in a list", () => {
     const array = [2, 3, 4, 5];
     let result = squareElements(array);
     let answer = [4, 9, 16, 25];
+    expect(result).toEqual(answer);
+});
+
+test("Distribute books to users and update status of delivery", () => {
+    const array = ["Emily", "Jack"];
+    let result = bookDistribution(array);
+    let answer = [
+        {
+            name: "Emily",
+            status: "Delivered"
+        },
+        {
+            name: "Jack",
+            status: "Delivered"
+        }
+    ];
+    expect(result).toEqual(answer);
+});
+
+test("Student planning schedule", () => {
+    const array = ["11:00 AM", "1:00 PM", "3:00 PM"];
+    let result = studentPlanningSchedule(array);
+    let answer = ["1:00 PM", "3:00 PM"];
+    expect(result).toEqual(answer);
+});
+
+test("Expense calculator", () => {
+    const array = { "groceries": 150, "dining out": 100, "transportation": 50, "entertainment": 80 };
+    let result = studentExpenseCalculator(array);
+    let answer = 380;
+    expect(result).toBe(answer);
+})
+
+test("Calculate Student grade", () => {
+    const array = [95, 78, 85, 60, 40];
+    let result = calculateStudentGrade(array);
+    let answer = ['A', 'C', 'B', 'D', 'F'];
+    expect(result).toEqual(answer);
+});
+
+test("Filter healthy items into the shopping list", () => {
+    const array = [
+        { name: 'Apples', category: 'Fruits', isHealthy: true },
+        { name: 'Potato Chips', category: 'Snacks', isHealthy: false },
+        { name: 'Carrots', category: 'Vegetables', isHealthy: true }
+    ];
+    let result = shoppingList(array);
+    let answer = ['Apples', 'Carrots'];
+    expect(result).toEqual(answer);
+});
+
+test("Get items whose total price is more than 100", () => {
+    const array =  [
+        { id: 1, items: [{ price: 25, quantity: 2 }, { price: 15, quantity: 3 }] },
+        { id: 2, items: [{ price: 100, quantity: 1 }, { price: 25, quantity: 2 }] },
+        { id: 3, items: [{ price: 130, quantity: 4 }, { price: 20, quantity: 3 }] },
+    ];
+    let result = customerOrderAnalyzer(array);
+    let answer = [
+        { id: 2, total: 125 },
+        { id: 3, total: 150 },
+    ];
     expect(result).toEqual(answer);
 });
